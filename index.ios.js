7ad3e4794bbc3d1ebd38128aae5ac6597c6cf67d
jvlobo <jvlg2002@hotmail.com>
2016-02-06 17:25:35 +0100

    App done

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ import React, {
   Image,
   ScrollView,
   RefreshControl,
+  ActivityIndicatorIOS,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -15,48 +16,147 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 var HomeChecker = React.createClass({
   getInitialState: function() {
     return {
-      borderColor: '#ef4a36',
-      iconName: 'reply',
       isRefreshing: false,
+      isLoading: true,
+
+      miguelBorderColor: styles.borderOut,
+      miguelIconName: 'reply',
+      miguelIconClass: styles.iconOut,
+      miguelHour: null,
+      miguelAgo: null,
+
+      loboBorderColor: styles.borderOut,
+      loboIconName: 'reply',
+      loboIconClass: styles.iconOut,
+      loboHour: null,
+      loboAgo: null,
     };
+  },
+
+  componentDidMount: function(){
+    this.checkStatus();
+  },
+
+  checkStatus: function(refreshing){
+    fetch('http://192.168.1.130:3000/status')
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+        this.setState({
+          isLoading: false,
+
+          miguelHour: res.miguel.time,
+          miguelAgo: res.miguel.timeago,
+          loboHour: res.lobo.time,
+          loboAgo: res.lobo.timeago,
+        });
+
+        if(res.miguel.status == 0){
+          this.setState({
+            miguelBorderColor: styles.borderOut,
+            miguelIconName: 'reply',
+            miguelIconClass: styles.iconOut,
+          });
+        }
+
+        if(res.lobo.status == 0){
+          this.setState({
+            loboBorderColor: styles.borderOut,
+            loboIconName: 'reply',
+            loboIconClass: styles.iconOut,
+          });
+        }
+
+        if(res.miguel.status == 1){
+          this.setState({
+            miguelBorderColor: styles.borderIn,
+            miguelIconName: 'share',
+            miguelIconClass: styles.iconIn,
+          });
+        }
+
+        if(res.lobo.status == 1){
+          this.setState({
+            loboBorderColor: styles.borderIn,
+            loboIconName: 'share',
+            loboIconClass: styles.iconIn,
+          });
+        }
+
+        if(res.miguel.status == 2){
+          this.setState({
+            miguelBorderColor: styles.borderUnknown,
+            miguelIconName: 'times',
+            miguelIconClass: styles.iconUnknown,
+          });
+        }
+
+        if(res.lobo.status == 2){
+          this.setState({
+            loboBorderColor: styles.borderUnknown,
+            loboIconName: 'times',
+            loboIconClass: styles.iconUnknown,
+          });
+        }
+
+        if(refreshing)
+          this.setState({isRefreshing: false});      
+    });
   },
 
   _onRefresh() {
     this.setState({isRefreshing: true});
-    setTimeout(() => {
-      // prepend 10 items
-
-      this.setState({
-        isRefreshing: false,
-      });
-    }, 5000);
+    this.checkStatus(true);    
   },
 
-  render() {
-    return (
-      <ScrollView
-        contentContainerStyle={styles.container}
-        style={styles.scrollView}
-        refreshControl={
-          <RefreshControl
-              refreshing={this.state.isRefreshing}
-              onRefresh={this._onRefresh}
-              tintColor="#ffffff"
-              colors={['#ffffff']}
-              progressBackgroundColor="#cccccc" />}
-          >
+  render: function() {
+    var contents;
+    if (this.state.isLoading) {
+      contents = (
+        <View style={ styles.loading }>
+          <Text style={ styles.loadingText }>Cargando</Text>
+          <ActivityIndicatorIOS />
+        </View>
+      )
+    } else {
+
+      contents = (
+        <View>
           <View style={[styles.personContainer, styles.miguelContainer]}>
-            <Image source={require('./images/miguel.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
-            <Text>20:30</Text>
-            <Icon name={this.state.iconName} size={30} color="#900" />
-            <Text>Two hours ago</Text>
+            <Image source={require('./images/miguel.jpg')} style={[styles.picture, this.state.miguelBorderColor]}/>
+            <View style={styles.hourContainer}>
+              <Icon name={this.state.miguelIconName} style={[styles.icon, this.state.miguelIconClass]}/>
+              <Text style={styles.hour}>{this.state.miguelHour}</Text>
+            </View>
+            <Text style={styles.timeAgo} >{this.state.miguelAgo}</Text>
           </View>
           <View style={styles.personContainer}>
-            <Image source={require('./images/lobo.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
-            <Text>20:30</Text>
-            <Text>Two hours ago</Text>
+            <Image source={require('./images/lobo.jpg')} style={[styles.picture, this.state.loboBorderColor]}/>
+            <View style={styles.hourContainer}>
+              <Icon name={this.state.loboIconName} style={[styles.icon, this.state.loboIconClass]}/>
+              <Text style={styles.hour}>{this.state.loboHour}</Text>
+            </View>
+            <Text style={styles.timeAgo} >{this.state.loboAgo}</Text>
           </View>
+        </View>
+      )
+    }
+
+    return (
+      <ScrollView
+          contentContainerStyle={styles.container}
+          style={styles.scrollView}
+          refreshControl={
+            <RefreshControl
+                refreshing={this.state.isRefreshing}
+                onRefresh={this._onRefresh}
+                tintColor="#ffffff"
+                colors={['#ffffff']}
+                progressBackgroundColor="#cccccc" />}
+            >
+        { contents }
       </ScrollView>
+      
     );
   }
 });
@@ -74,13 +174,71 @@ const styles = StyleSheet.create({
     backgroundColor: '#cccccc',
   },
 
+  loading: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  loadingText: {
+    fontSize: 14,
+    marginBottom: 20
+  },
+
   personContainer: {
     flex: 1,
+    flexDirection: 'column',
     alignItems: 'center',
   },
 
   miguelContainer: {
     justifyContent: 'center',
+    marginBottom: 20,
+  },
+
+  borderOut: {
+    borderColor: '#f24233',
+  },
+
+  borderIn: {
+    borderColor: '#06918c',
+  },
+
+  borderUnknown: {
+    borderColor: '#bbbbbb',
+  },
+
+  hourContainer: {
+    flexDirection: 'row',
+  },
+
+  icon: {
+    fontSize: 20,
+    position: 'absolute',
+    left: -15,
+  },
+
+  iconOut: {
+    color: '#f24233',
+  },
+
+  iconIn: {
+    color: '#06918c',
+  },
+
+  iconUnknown: {
+    color: '#bbbbbb',
+  },
+
+  hour: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+
+  timeAgo: {
+    color: '#bbbbbb',
   },
 
   picture: {
