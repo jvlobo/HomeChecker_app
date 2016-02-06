c133079a94fb415a51fc0d6adfdf21ba66b7f13d
jvlobo <jvlg2002@hotmail.com>
2016-02-06 15:57:31 +0100

    Pull to refresh done

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,9 @@ import React, {
   StyleSheet,
   Text,
   View,
-  Image,  
+  Image,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -15,24 +17,46 @@ var HomeChecker = React.createClass({
     return {
       borderColor: '#ef4a36',
       iconName: 'reply',
+      isRefreshing: false,
     };
+  },
+
+  _onRefresh() {
+    this.setState({isRefreshing: true});
+    setTimeout(() => {
+      // prepend 10 items
+
+      this.setState({
+        isRefreshing: false,
+      });
+    }, 5000);
   },
 
   render() {
     return (
-      <View style={styles.container}>
-        <View style={[styles.personContainer, styles.miguelContainer]}>
-          <Image source={require('./images/miguel.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
-          <Text>20:30</Text>
-          <Icon name={this.state.iconName} size={30} color="#900" />
-          <Text>Two hours ago</Text>
-        </View>
-        <View style={styles.personContainer}>
-          <Image source={require('./images/lobo.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
-          <Text>20:30</Text>
-          <Text>Two hours ago</Text>
-        </View>
-      </View>
+      <ScrollView
+        contentContainerStyle={styles.container}
+        style={styles.scrollView}
+        refreshControl={
+          <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this._onRefresh}
+              tintColor="#ffffff"
+              colors={['#ffffff']}
+              progressBackgroundColor="#cccccc" />}
+          >
+          <View style={[styles.personContainer, styles.miguelContainer]}>
+            <Image source={require('./images/miguel.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
+            <Text>20:30</Text>
+            <Icon name={this.state.iconName} size={30} color="#900" />
+            <Text>Two hours ago</Text>
+          </View>
+          <View style={styles.personContainer}>
+            <Image source={require('./images/lobo.jpg')} style={[styles.picture, {borderColor: this.state.borderColor}]}/>
+            <Text>20:30</Text>
+            <Text>Two hours ago</Text>
+          </View>
+      </ScrollView>
     );
   }
 });
@@ -44,6 +68,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fafafa',
+  },
+
+  scrollView: {
+    backgroundColor: '#cccccc',
   },
 
   personContainer: {
